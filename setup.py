a3b71f5adeee211191d3cf74beb484bac8968b4a
M Chimiste <37678080+M-Chimiste@users.noreply.github.com>
2025-05-02 08:16:54 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="arxiv-harvester",
-    version="0.1.0",
+    version="0.1.1",
     author="arxiv-harvester Contributors",
     description="A rate-limit-compliant OAI-PMH harvester for ArXiv papers",
     long_description=long_description,
