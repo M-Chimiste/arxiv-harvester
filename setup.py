cc0c366ab3af85722cc8ad6641baaa20b3bb65a7
M Chimiste <37678080+M-Chimiste@users.noreply.github.com>
2025-05-02 07:45:19 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     description="A rate-limit-compliant OAI-PMH harvester for ArXiv papers",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/arxiv-harvester",
+    url="https://github.com/M-Chimiste/arxiv-harvester",
     packages=find_packages(),
     classifiers=[
         "Development Status :: 3 - Alpha",
